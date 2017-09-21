19c56da13ee9ccdc685d3b37323fab18542a67b0
tsii antibess <tsiibest@gmail.com>
2017-09-21 02:45:42 +0100

    minor pep8 guideline edits

--- media.py
+++ media.py
@@ -1,10 +1,10 @@
-import webbrowser
-    
     
 class Movie():
-    """This class provides a way to store movie related information"""
-
-    VALID_RATINGS = ["G", "PG", "PG-13", "R"]
+    """ This class provides a way to store movie related information.
+    It contains the __init__ method which represents the constructor of
+    the class and it serves as a blueprint to create different movie objects"""
+    
+	VALID_RATINGS = ["G", "PG", "PG-13", "R"]
     
     def __init__(self, movie_title, movie_storyline, poster_image,
                  trailer_youtube):
@@ -13,6 +13,4 @@ class Movie():
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
-    
-
     
